dec5ed87b0179c148a2af179ee59af3898edb01e
--delete <--delete>
2023-12-12 11:30:29 +0600

    custom font setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "keep-react"
+import "./App.css"
 
 function App() {
 
   return (
     <>
       <Button size="md">Default</Button>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus quibusdam beatae quas incidunt voluptatem est dolore quia, eius, recusandae repellendus nostrum laboriosam distinctio quod officia quidem. Dolor omnis vero quisquam? Sapiente tempore nam molestiae porro dolorem ducimus quisquam esse voluptas dolorum ipsam accusantium rem ad aperiam earum officiis qui, voluptates explicabo optio. Dolores facere molestias cumque incidunt minima quidem vero voluptatem possimus voluptate, debitis laboriosam dolor corporis quisquam facilis, alias eum et praesentium illo? Ullam unde odit, voluptatibus aspernatur non possimus quae voluptates mollitia, ad autem perferendis quo fugiat libero voluptas atque sequi. Minus pariatur enim officiis deleniti! Unde.</p>
     </>
   )
 }
